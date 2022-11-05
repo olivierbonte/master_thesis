1fb9379204133cba0a93b70874ace9e5cd27b234
olivierbonte <olivier.bonte@ugent.be>
2022-11-05 22:33:23 +0100

    Add Inverse Distance Weighing

--- Zwalm_forcings_preprocess.py
+++ Zwalm_forcings_preprocess.py
@@ -2,6 +2,15 @@ import pandas as pd
 from pathlib import Path
 import datetime
 import numpy as np
+import os
+import geopandas as gpd
+import shapely
+
+
+
+##########################################################
+# Original processing: only 1 measurement point considered
+##########################################################
 
 dateparse = lambda x: datetime.datetime.strptime(x, "%d/%m/%Y %H:%M:%S")
 data_p = pd.read_csv(Path("data\Zwalm_data\MaarkeKerkem_Neerslag_1h_geaccumuleerd.csv"), parse_dates = ['Datum'],
@@ -63,5 +72,74 @@ for i in indeces_filled:
      +  data_zwalm_hourly['EP'].iloc[i-1].values)
 
 data_zwalm_hourly.to_csv('data/Zwalm_data/zwalm_forcings_flow.csv', index = False)
- 
+
+#also make a dataset where flow is aggregated on a daily basis
+#timeseries_daily = pd.date_range(start = start_date_00, end = end_date_23, freq = 'D')
+flow_zalm_hourly = data_zwalm_hourly[['Time','Flow']]
+flow_zalm_hourly = flow_zalm_hourly.set_index('Time')
+def custom_nanmean(arraylike):
+    return np.nanmean(arraylike)
+flow_zwalm_daily = flow_zalm_hourly.resample('1D').apply(custom_nanmean)
+flow_zwalm_daily = flow_zwalm_daily.reset_index()
+flow_zwalm_daily.to_csv("data/Zwalm_data/zwalm_flow_daily.csv", index = False)
+
+###########################################################
+# Extended processing: multiple measuring points considered
+###########################################################
+## Step 1: Inverse Distance Weighing
+list_longitude_WGS84 = []
+list_latitude_WGS84 = []
+list_station_names = []
+measurements_file = Path("data\Zwalm_data\waterinfo_measurements_rain")
+for filename in os.listdir(measurements_file):
+    if filename[-3:] == 'csv':
+        #print(filename)
+        info_station = pd.read_csv(measurements_file/filename, sep = ';', 
+        nrows = 4, header=None, index_col= 0)
+        latitude_WGS84 = info_station.loc[['#station_latitude']].values.flatten()
+        list_latitude_WGS84.append(latitude_WGS84.astype(dtype = np.float64).tolist()[0])
+        longitude_WGS84 = info_station.loc[['#station_longitude']].values.flatten()
+        list_longitude_WGS84.append(longitude_WGS84.astype(dtype = np.float64).tolist()[0])
+        list_station_names.append(info_station.loc[['#station_name']].values.tolist()[0])
+df_stations = pd.DataFrame(
+    {'Station': list_station_names,
+     'Latitude':list_latitude_WGS84,
+     'Longitude':list_longitude_WGS84})
+gdf_stations = gpd.GeoDataFrame(
+    df_stations, 
+    geometry=gpd.points_from_xy(df_stations['Longitude'], df_stations['Latitude']))# type: ignore 
+gdf_stations = gdf_stations.set_crs('EPSG:4326')
+#testing for 1: extract coordinates! => later in a function
+# info_station = pd.read_csv(measurements_file/"Zingem_P_Neerslag.csv", sep = ';', 
+#     nrows = 4, header=None, index_col= 0)
+# latitude_WGS84 = info_station.loc[['#station_latitude']].values.flatten()
+# latitude_WGS84 = latitude_WGS84.astype(dtype = np.float64)
+# longitude_WGS84 = info_station.loc[['#station_longitude']].values.flatten()
+# longitude_WGS84 = longitude_WGS84.astype(dtype = np.float64)
+
+#import shapefile of Zwalm: option 1 = EPSG 4326
+#zwalm_WGS84 = gpd.read_file(Path("data\Zwalm_shape\zwalm_shapefile_emma.shp"))
+#zwalm_WGS84_centroid = zwalm_WGS84['geometry'].centroid
+#https://en.wikipedia.org/wiki/Centroid
+#gdf_stations['distance'] = gdf_stations['geometry'].distance(zwalm_WGS84_centroid.iloc[0])
+
+#import shapefile of Zwalm: option 2 = EPSG 31370 = BETTER for distances!!
+zwalm_lambert = gpd.read_file(Path("data\Zwalm_shape\zwalm_shapefile_emma_31370.shp"))
+zwalm_lambert_centroid = zwalm_lambert['geometry'].centroid
+gdf_stations_lambert = gdf_stations.to_crs('EPSG:31370').copy()#type:ignore
+gdf_stations_lambert['distance'] = gdf_stations_lambert['geometry'].distance(zwalm_lambert_centroid.iloc[0])#type: ignore
+
+#inverse distance weighing
+b = 1
+#gdf_stations['IDW_factor_linear'] = gdf_stations['distance']**(-b)/sum(gdf_stations['distance']**(-b))
+gdf_stations_lambert['IDW_factor_linear'] = gdf_stations_lambert['distance']**(-b)/sum(gdf_stations_lambert['distance']**(-b))
+b =2
+gdf_stations_lambert['IDW_factor_quadratic'] = gdf_stations_lambert['distance']**(-b)/sum(gdf_stations_lambert['distance']**(-b))
+b = 3
+gdf_stations_lambert['IDW_factor_cubic'] = gdf_stations_lambert['distance']**(-b)/sum(gdf_stations_lambert['distance']**(-b))
+#factors -> csv
+gdf_stations_lambert.to_csv(Path("data\Zwalm_data\zwalm_idw.csv"))
+
+
+## Step 2: reading in the data
 
