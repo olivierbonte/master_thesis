4fec9990f3ee6fb6c3c4b848f1d8980d10cce595
olivierbonte <olivier.bonte@ugent.be>
2022-12-20 15:04:53 +0100

    add numba to PDM for speedup

--- functions/PDM.py
+++ functions/PDM.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numba import jit
 import pandas as pd
 import os
 from pathlib import Path
@@ -10,7 +11,7 @@ if pad.name != "Python":
 from functions.performance_metrics import NSE, mNSE
 
 
-def PDM(P, EP, t, area, deltat, deltatout, parameters):
+def PDM(P:np.ndarray, EP:np.ndarray, t, area:np.float32, deltat, deltatout, parameters):
     """Probability Distributed Model from "The PDM rainfall-runoff model" by Moore (2007).
     References to equations in this paper are made with their respective equation number. 
     From Appendix A, only formula referring to the Pareto Distribution are used.
@@ -21,10 +22,10 @@ def PDM(P, EP, t, area, deltat, deltatout, parameters):
         rainfall intensity [mm/h]. Preferably provided in np.float32
     EP: numpy.array
         evapotranspiration [mm/h]. Preferably provided in np.float32
-    t: datetime64[ns]
+    t: numpy.array, datetime64[ns]
         time stamps when P and EP are collected. Must start at 00:00 and end at 23:00 for hourly data.
-    area: float
-        area of catchment [km^2]
+    area: np.float32
+        area of catchment [km^2]. Crucial that this is NOT an array because this will terminate numba compiling! 
     deltat: float or int
         internal time resolution used by the model. Note that this time resolution
         should also be present in forcings (P and EP) [h]
@@ -76,8 +77,8 @@ def PDM(P, EP, t, area, deltat, deltatout, parameters):
         EP = EP.flatten()
     
     #check if frocings are of same length
-    t_length = len(P)
-    t_length_ep = len(EP)
+    t_length = np.int32(len(P))
+    t_length_ep = np.int32(len(EP))
     if t_length != t_length_ep:
         raise ValueError("""p and ep do not have the same length,
         Provide p and ep of the same length""")
@@ -110,108 +111,110 @@ def PDM(P, EP, t, area, deltat, deltatout, parameters):
     else:
         omega_0 = (k1*(delta1star - 1)-k2*(delta2star -1))/(k2 - k1)
         omega_1 = (k2*(delta2star - 1)*delta1star - k1*(delta1star - 1)*delta2star)/(k2-k1)
-    
-    #initialisations of variables:
-    Eiacc = np.zeros(t_length, dtype = np.float32) #actual evaporation [mm/h], cf (8)
-    V = np.zeros(t_length, dtype = np.float32) #volume or basin direct runoff per unit area over interval [mm]
-    qd = np.zeros(t_length, dtype = np.float32) #Flow of direct runoff [mm/h]
-    di = np.zeros(t_length, dtype = np.float32) #drainage to groundwater storgae [mm/h]
-    Cstar = np.zeros(t_length, dtype = np.float32) #Critical storage capacity [mm]
-    S1 = np.zeros(t_length, dtype = np.float32) #Storage in soil moisture reservoir [mm]
-    S3 = np.zeros(t_length, dtype = np.float32) #Storage in groundwater reservoir [mm]
-    pi = np.zeros(t_length, dtype = np.float32) #Net rainfaill [mm]
-    qb = np.zeros(t_length, dtype = np.float32) #Baseflow going from groundwater storage S3 [mm/h]
-    qbm3s = np.zeros(t_length, dtype = np.float32) #Baseflow going from groundwater storage S3 [m^3/s]
-    qs = np.zeros(t_length, dtype = np.float32) #Surface runoff from surface storage S2 [mm]
-    qsm3s = np.zeros(t_length, dtype = np.float32) #Surface runoff from surface storage S2 [m^3/s]
-    qmodm3s = np.zeros((t_length + int(tdly/deltat),1), dtype = np.float32) #Total modelled flow leaving the catchment [m^3/s] 
+    @jit(nopython = True)
+    def numba_start():
+        #initialisations of variables:
+        Eiacc = np.zeros(t_length, dtype = np.float32) #actual evaporation [mm/h], cf (8)
+        V = np.zeros(t_length, dtype = np.float32) #volume or basin direct runoff per unit area over interval [mm]
+        qd = np.zeros(t_length, dtype = np.float32) #Flow of direct runoff [mm/h]
+        di = np.zeros(t_length, dtype = np.float32) #drainage to groundwater storgae [mm/h]
+        Cstar = np.zeros(t_length, dtype = np.float32) #Critical storage capacity [mm]
+        S1 = np.zeros(t_length, dtype = np.float32) #Storage in soil moisture reservoir [mm]
+        S3 = np.zeros(t_length, dtype = np.float32) #Storage in groundwater reservoir [mm]
+        pi = np.zeros(t_length, dtype = np.float32) #Net rainfaill [mm]
+        qb = np.zeros(t_length, dtype = np.float32) #Baseflow going from groundwater storage S3 [mm/h]
+        qbm3s = np.zeros(t_length, dtype = np.float32) #Baseflow going from groundwater storage S3 [m^3/s]
+        qs = np.zeros(t_length, dtype = np.float32) #Surface runoff from surface storage S2 [mm]
+        qsm3s = np.zeros(t_length, dtype = np.float32) #Surface runoff from surface storage S2 [m^3/s]
+        qmodm3s = np.zeros((t_length + int(tdly/deltat),1), dtype = np.float32) #Total modelled flow leaving the catchment [m^3/s] 
 
 
-    #################################
-    # Initialisation of state variables
-    ##################################
-    i = 0
-    imax = t_length 
-    S1[i] = Smax/2 #So choice the intialise at 1/2 of max capacity
-    S3[i] = 0.001 #random choice? Does not matter to much, cf burnup
-    # Critical capacity
-    Cstar[i] = cmin + (cmax - cmin)*(1 - ((Smax-S1[i])/(Smax - cmin))**(1/(b_param+1))) #Appendix A
-    if Cstar[i] > cmax:
-        Cstar[i] = cmax
-    elif Cstar[i] < 0: #e.g. when completely dry! Cstar goes below cmin!
-        Cstar[i] = 0
-
-    ######################################
-    # Running model through all time steps
-    ######################################
-    i = i + 1
-    istart = i
-    for i in np.arange(istart,imax):
-        ### PROBABILITY DISTRIBUTED SOIL-MOISTURE STORAGE S1 ###
-        #Acutal Evaporation
-        Eiacc[i] = EP[i]*(1-((Smax - S1[i-1])/Smax)**be) # (8)
-        #Drainage to groundwater
-        if S1[i-1] > St:
-            di[i] = (1/kg)*((S1[i-1]- St)**bg) # (10)
-        else:
-            di[i] = 0
-        #Net rainfall
-        pi[i] = P[i] - Eiacc[i] - di[i] # (14)
-
-        condition = 0
-        if pi[i] > 0: #net rainfall
-            condition = condition + 1
-        #if C[i-1] > cmin #Original, I believe non correct
-        if Cstar[i-1] + pi[i]*deltat > cmin: #cf. Moore en bell (2002) OWN IDEA!!!
-            condition = condition + 1
-
-        if condition == 2:
-            Cstar_t = Cstar[i-1]
-            Cstar_t_plus_deltat = Cstar[i-1] + pi[i]*deltat #  (5)
-            S1t = cmin + (Smax - cmin)*(1 - ((cmax - Cstar_t)/(cmax - cmin))**(b_param+1)) # Appendix A
-            if Cstar_t_plus_deltat < cmax:
-                S1t_plus_deltat = cmin + (Smax - cmin)*(1 - ((cmax - Cstar_t_plus_deltat)/(cmax - cmin))**(b_param+1))
-                V[i] = pi[i]*deltat - (S1t_plus_deltat - S1t) #Appendix A
-            else: #so when Cstar[i-1] + pi[i]*deltat > cmax
-                V[i] = pi[i]*deltat - (Smax - S1t) # (17)
-        else:
-            V[i] = 0 #so no runoff generated!
-        #not sure if the following is necessary
-        if V[i] < 0:
-            V[i] = 0
-        
-        #Update S1
-        S1[i] = S1[i-1] + pi[i]*deltat - V[i] # (16)
-        if S1[i] > Smax:
-            S1[i] = Smax
-        elif S1[i] < 0:
-            S1[i] = 0
-        
-        #Update Cstar: NOT based on (5) here (not 100% sure why),
-        # but on Appendix A (relates S1 to C)
-        Cstar[i] = cmin + (cmax - cmin)*(1 - ((Smax - S1[i])/(Smax - cmin))**(1/(b_param+1)))
+        #################################
+        # Initialisation of state variables
+        ##################################
+        i = 0
+        imax = t_length 
+        S1[i] = Smax/2 #So choice the intialise at 1/2 of max capacity
+        S3[i] = 0.001 #random choice? Does not matter to much, cf burnup
+        # Critical capacity
+        Cstar[i] = cmin + (cmax - cmin)*(1 - ((Smax-S1[i])/(Smax - cmin))**(1/(b_param+1))) #Appendix A
         if Cstar[i] > cmax:
             Cstar[i] = cmax
-        elif Cstar[i] <= 0: #Cstar can be smaller than cmin! e.g. if completely dry
+        elif Cstar[i] < 0: #e.g. when completely dry! Cstar goes below cmin!
             Cstar[i] = 0
 
-        ### GROUNDWATER STORAGE S3 ###
-        S3[i] = S3[i-1] - 1/(3*kb*S3[i-1]**2)*(np.exp(-3*deltat*kb*S3[i-1]**2)-1)*(di[i]-kb*S3[i-1]**3) # (24)
-        qb[i] = kb*S3[i]**3
-        if qb[i] < 0:
-            qb[i] = 0
-        qbm3s[i] = qb[i]*aream2/(1000*3600) #mm -> m, h -> s
+        ######################################
+        # Running model through all time steps
+        ######################################
+        i = i + 1
+        istart = i
+        for i in np.arange(istart,imax):
+            ### PROBABILITY DISTRIBUTED SOIL-MOISTURE STORAGE S1 ###
+            #Acutal Evaporation
+            Eiacc[i] = EP[i]*(1-((Smax - S1[i-1])/Smax)**be) # (8)
+            #Drainage to groundwater
+            if S1[i-1] > St:
+                di[i] = (1/kg)*((S1[i-1]- St)**bg) # (10)
+            else:
+                di[i] = 0
+            #Net rainfall
+            pi[i] = P[i] - Eiacc[i] - di[i] # (14)
 
-        ### SURFACE STORAGE S2 ###
-        qd[i] = V[i]/deltat #added on own behalve
-        if i > 1:
-            qs[i] = -delta1*qs[i-1] - delta2*qs[i-2] +omega_0*qd[i] + omega_1*qd[i-1]
-            qsm3s[i] = qs[i]*aream2/(1000*3600) #mm -> m, h -> s
-    
-        ### TOTAL FLOW ###
-        qmodm3s[i+int(tdly)] = qbm3s[i] + qsm3s[i] + qconst
-    
+            condition = 0
+            if pi[i] > 0: #net rainfall
+                condition = condition + 1
+            #if C[i-1] > cmin #Original, I believe non correct
+            if Cstar[i-1] + pi[i]*deltat > cmin: #cf. Moore en bell (2002) OWN IDEA!!!
+                condition = condition + 1
 
+            if condition == 2:
+                Cstar_t = Cstar[i-1]
+                Cstar_t_plus_deltat = Cstar[i-1] + pi[i]*deltat #  (5)
+                S1t = cmin + (Smax - cmin)*(1 - ((cmax - Cstar_t)/(cmax - cmin))**(b_param+1)) # Appendix A
+                if Cstar_t_plus_deltat < cmax:
+                    S1t_plus_deltat = cmin + (Smax - cmin)*(1 - ((cmax - Cstar_t_plus_deltat)/(cmax - cmin))**(b_param+1))
+                    V[i] = pi[i]*deltat - (S1t_plus_deltat - S1t) #Appendix A
+                else: #so when Cstar[i-1] + pi[i]*deltat > cmax
+                    V[i] = pi[i]*deltat - (Smax - S1t) # (17)
+            else:
+                V[i] = 0 #so no runoff generated!
+            #not sure if the following is necessary
+            if V[i] < 0:
+                V[i] = 0
+            
+            #Update S1
+            S1[i] = S1[i-1] + pi[i]*deltat - V[i] # (16)
+            if S1[i] > Smax:
+                S1[i] = Smax
+            elif S1[i] < 0:
+                S1[i] = 0
+            
+            #Update Cstar: NOT based on (5) here (not 100% sure why),
+            # but on Appendix A (relates S1 to C)
+            Cstar[i] = cmin + (cmax - cmin)*(1 - ((Smax - S1[i])/(Smax - cmin))**(1/(b_param+1)))
+            if Cstar[i] > cmax:
+                Cstar[i] = cmax
+            elif Cstar[i] <= 0: #Cstar can be smaller than cmin! e.g. if completely dry
+                Cstar[i] = 0
+
+            ### GROUNDWATER STORAGE S3 ###
+            S3[i] = S3[i-1] - 1/(3*kb*S3[i-1]**2)*(np.exp(-3*deltat*kb*S3[i-1]**2)-1)*(di[i]-kb*S3[i-1]**3) # (24)
+            qb[i] = kb*S3[i]**3
+            if qb[i] < 0:
+                qb[i] = 0
+            qbm3s[i] = qb[i]*aream2/(1000*3600) #mm -> m, h -> s
+
+            ### SURFACE STORAGE S2 ###
+            qd[i] = V[i]/deltat #added on own behalve
+            if i > 1:
+                qs[i] = -delta1*qs[i-1] - delta2*qs[i-2] +omega_0*qd[i] + omega_1*qd[i-1]
+                qsm3s[i] = qs[i]*aream2/(1000*3600) #mm -> m, h -> s
+        
+            ### TOTAL FLOW ###
+            qmodm3s[i+int(tdly)] = qbm3s[i] + qsm3s[i] + qconst
+        return qmodm3s, Cstar, S1, S3
+
+    qmodm3s, Cstar, S1, S3 = numba_start()
     #tmod output: idea to only give output till last timestamp of input!
     freq_hour = str(deltatout) + 'H'
     tmod = pd.date_range(t[0],t[-1], freq = freq_hour) 
@@ -241,6 +244,7 @@ def PDM(P, EP, t, area, deltat, deltatout, parameters):
     S3 = S3.reshape((-1,int(deltatout/deltat)))
     S3 = np.nanmean(S3, axis = 1)
     S3 = S3[:len(tmod)] 
+        
     
     pd_out = pd.DataFrame(
         {"qmodm3s":qmodm3s,
