dd8fb249ae5a1b8bb8c5625b1b4d72fa38b9419e
olivierbonte <olivier.bonte@ugent.be>
2023-03-01 16:35:54 +0100

    add zenodo download

--- preprocessing_files/shapefile_conversion.py
+++ preprocessing_files/shapefile_conversion.py
@@ -3,14 +3,18 @@
 import geopandas as gpd
 from pathlib import Path
 import os
+import zipfile
 pad = Path(os.getcwd())
 if pad.name == "preprocessing_files":
     pad_correct = pad.parent
     os.chdir(pad_correct)
-zwalm_gpd_subbasins = gpd.read_file("data_github\OS266.shp")
-circumference_zwalm = zwalm_gpd_subbasins.unary_union
+os.system('cmd /c zenodo_get 10.5281/zenodo.7688784')
+with zipfile.ZipFile("data_github.zip", 'r') as zip_ref:
+    zip_ref.extractall('data_github')
+zwalm_gpd_subbasins = gpd.read_file("data_github/OS266.shp")
+circumference_zwalm = zwalm_gpd_subbasins.unary_union#type:ignore
 d = {'PolygonId': 15, 'Area': circumference_zwalm.area, 'Subbasin':0,'geometry':circumference_zwalm}
-zwalm_gpd = zwalm_gpd_subbasins.append(d, ignore_index= True)
+zwalm_gpd = zwalm_gpd_subbasins.append(d, ignore_index= True)#type:ignore
 zwalm_gpd.crs = 31370 # set crs: Lambert72
 zwalm_shape_epsg31370 = zwalm_gpd.iloc[[len(zwalm_gpd)-1]]
 if not os.path.exists('data/Zwalm_shape'):
