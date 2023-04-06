8c232f26fd81a1d113e3e4894817b046a6f5b604
olivierbonte <olivier.bonte@ugent.be>
2023-04-06 16:02:32 +0200

    add ensembe plot, modify tf history plot

--- functions/plotting_functions.py
+++ functions/plotting_functions.py
@@ -129,13 +129,16 @@ def plot_continuous_raster(
         fig = im
     return fig, ax
 
-def plot_tf_history(history):
+def plot_tf_history(history, plot_object = "loss"):
     """
     Function to plot the training and validation loss of training with tensorflow
 
     Inputs
     ------
     history: keras.callbacks.History
+
+    plot_object: string
+        To be plotted for both training and validation (if the latter exitst)
     
     Returns
     ------
@@ -146,7 +149,27 @@ def plot_tf_history(history):
     
     """
     fig, ax = plt.subplots()
-    ax.plot(range(len(history.history["loss"])),history.history["loss"],label = 'Training loss')
-    ax.plot(range(len(history.history["loss"])),history.history["val_loss"], label = 'Validation loss')
+    metrics = history.history.keys()
+    if plot_object in metrics:
+        ax.plot(range(len(history.history[plot_object])),history.history[plot_object],label = f'Training {plot_object}')
+    else:
+        raise ValueError(f'The training metric {plot_object} is not defined for the training data')
+    val_metric = 'val_' + plot_object
+    if val_metric in metrics:
+        ax.plot(range(len(history.history[val_metric])),history.history[val_metric], label = f'Validation {plot_object}')
     ax.legend()
+    return fig, ax
+
+def ensemble_plot(Cstar, tfull, out_dict, n_std = 2, fig = None, ax = None):
+    if (fig == None) and (ax == None):
+        fig, ax = plt.subplots(figsize = (9,7))
+    Cstar[tfull].plot(ax =ax)
+    ax.plot(out_dict['t_train'], out_dict['mean_y_train_hat'], label = 'Mean training set')#type:ignore
+    ax.plot(out_dict['t_test'], out_dict['mean_y_test_hat'], label = 'Mean test set')#type:ignore
+    ax.fill_between(out_dict['t_train'], out_dict['mean_y_train_hat'] - n_std*out_dict['sd_y_train_hat'],#type:ignore
+                    out_dict['mean_y_train_hat'] + n_std*out_dict['sd_y_train_hat'], color = 'grey', alpha = 0.5)
+    ax.fill_between(out_dict['t_test'], out_dict['mean_y_test_hat'] - n_std*out_dict['sd_y_test_hat'],#type:ignore
+                    out_dict['mean_y_test_hat'] + n_std*out_dict['sd_y_test_hat'], color= 'grey', alpha = 0.5)
+    ax.legend()#type:ignore
+    ax.set_title(r'$\mu \pm' + f'{n_std}'+ r'\sigma$')#type:ignore
     return fig, ax
