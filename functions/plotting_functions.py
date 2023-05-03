7dc8479033461c56fb46564dc869d38a02037b78
olivierbonte <olivier.bonte@ugent.be>
2023-05-03 10:05:53 +0200

    add FDC plot

--- functions/plotting_functions.py
+++ functions/plotting_functions.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mplcolors
-from matplotlib.colors import LightSource
 import matplotlib.patches as mpatches
+from matplotlib.colors import LightSource
+from statsmodels.distributions.empirical_distribution import ECDF
+
 
 def plot_discrete_raster(
     fig, ax, arr, labels, bounds, cmap="cividis", norm=None, *args, **kwargs
@@ -55,8 +57,10 @@ def plot_discrete_raster(
         )
         for i in range(len(values))
     ]
-    ax.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
+    ax.legend(handles=patches, bbox_to_anchor=(
+        1.05, 1), loc=2, borderaxespad=0.0)
     return fig, ax
+
 
 def plot_continuous_raster(
     fig,
@@ -64,7 +68,7 @@ def plot_continuous_raster(
     arr,
     bounds,
     cmap="cividis",
-    hillshade = False,
+    hillshade=False,
     norm=None,
     colorbar=True,
     ticks=None,
@@ -129,7 +133,8 @@ def plot_continuous_raster(
         fig = im
     return fig, ax
 
-def plot_tf_history(history, plot_object = "loss"):
+
+def plot_tf_history(history, plot_object="loss"):
     """
     Function to plot the training and validation loss of training with tensorflow
 
@@ -139,37 +144,78 @@ def plot_tf_history(history, plot_object = "loss"):
 
     plot_object: string
         To be plotted for both training and validation (if the latter exitst)
-    
+
     Returns
     ------
     fig: matplotlib.figure.Figure
 
     ax: matplotlib.pyplot.axis
 
-    
+
     """
     fig, ax = plt.subplots()
     metrics = history.history.keys()
     if plot_object in metrics:
-        ax.plot(range(len(history.history[plot_object])),history.history[plot_object],label = f'Training {plot_object}')
+        ax.plot(range(len(history.history[plot_object])),
+                history.history[plot_object], label=f'Training {plot_object}')
     else:
-        raise ValueError(f'The training metric {plot_object} is not defined for the training data')
+        raise ValueError(
+            f'The training metric {plot_object} is not defined for the training data')
     val_metric = 'val_' + plot_object
     if val_metric in metrics:
-        ax.plot(range(len(history.history[val_metric])),history.history[val_metric], label = f'Validation {plot_object}')
+        ax.plot(range(len(history.history[val_metric])),
+                history.history[val_metric], label=f'Validation {plot_object}')
     ax.legend()
     return fig, ax
 
-def ensemble_plot(Cstar, tfull, out_dict, n_std = 2, fig = None, ax = None):
+
+def ensemble_plot(Cstar, tfull, out_dict, n_std=2, fig=None, ax=None):
     if (fig == None) and (ax == None):
-        fig, ax = plt.subplots(figsize = (9,7))
-    Cstar[tfull].plot(ax =ax)
-    ax.plot(out_dict['t_train'], out_dict['mean_y_train_hat'], label = 'Mean training set')#type:ignore
-    ax.plot(out_dict['t_test'], out_dict['mean_y_test_hat'], label = 'Mean test set')#type:ignore
-    ax.fill_between(out_dict['t_train'], out_dict['mean_y_train_hat'] - n_std*out_dict['sd_y_train_hat'],#type:ignore
-                    out_dict['mean_y_train_hat'] + n_std*out_dict['sd_y_train_hat'], color = 'grey', alpha = 0.5)
-    ax.fill_between(out_dict['t_test'], out_dict['mean_y_test_hat'] - n_std*out_dict['sd_y_test_hat'],#type:ignore
-                    out_dict['mean_y_test_hat'] + n_std*out_dict['sd_y_test_hat'], color= 'grey', alpha = 0.5)
-    ax.legend()#type:ignore
-    ax.set_title(r'$\mu \pm' + f'{n_std}'+ r'\sigma$')#type:ignore
+        fig, ax = plt.subplots(figsize=(9, 7))
+    Cstar[tfull].plot(ax=ax)
+    ax.plot(out_dict['t_train'], out_dict['mean_y_train_hat'],  # type:ignore
+            label='Mean training set')
+    ax.plot(out_dict['t_test'], out_dict['mean_y_test_hat'],  # type:ignore
+            label='Mean test set')
+    ax.fill_between(out_dict['t_train'], out_dict['mean_y_train_hat'] - n_std * out_dict['sd_y_train_hat'],  # type:ignore
+                    out_dict['mean_y_train_hat'] + n_std * out_dict['sd_y_train_hat'], color='grey', alpha=0.5)
+    ax.fill_between(out_dict['t_test'], out_dict['mean_y_test_hat'] - n_std * out_dict['sd_y_test_hat'],  # type:ignore
+                    out_dict['mean_y_test_hat'] + n_std * out_dict['sd_y_test_hat'], color='grey', alpha=0.5)
+    ax.legend()  # type:ignore
+    ax.set_title(r'$\mu \pm' + f'{n_std}' + r'\sigma$')  # type:ignore
+    return fig, ax
+
+
+def plot_FDC(Q, fig=None, ax=None, label=None, cutoff=0.02, cutoff_bool=True):
+    """
+    Plotting the flow duration curve (flow exceedance probability curve) for a time series of flows (without NaNs)
+
+    Parameters
+    ----------
+    Q: np.array
+        array with flows values
+    fig: matplotlib.figure.Figure
+
+    ax: matplotlib.pyplot.axis
+
+    Returns
+    -------
+    fig: matplotlib.figure.Figure
+
+    ax: matplotlib.pyplot.axis
+    """
+    if not fig and not ax:
+        fig, ax = plt.subplots()
+    Q_nonan = Q[~np.isnan(Q)]
+    Q_sorted = np.sort(Q_nonan)
+    CDF_distribution = ECDF(Q_sorted)
+    CDF = CDF_distribution(Q_sorted)
+    ax.plot(1 - CDF, Q_sorted, label=label)  # type:ignore
+    if cutoff_bool:
+        ax.vlines(cutoff, ax.get_ylim()[0], ax.get_ylim()[  # type:ignore
+            1], color='black', label=f'$p$ = {cutoff}')
+    ax.set_yscale('log')  # type:ignore
+    ax.set_xlabel('Flow exceedance probability')  # type:ignore
+    ax.set_ylabel('$Q$ [m$^3$/s]')  # type:ignore
+    ax.legend()  # type:ignore
     return fig, ax
