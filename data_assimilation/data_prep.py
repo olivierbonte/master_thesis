0291adb91206270935f28699c8b0f76b17b2e94a
olivierbonte <olivier.bonte@ugent.be>
2023-05-17 15:47:38 +0200

    add no forest and remove lfs

--- data_assimilation/data_prep.py
+++ data_assimilation/data_prep.py
@@ -31,6 +31,16 @@ y_linreg_hat_test_nt = pd.read_pickle(
 y_linreg_hat_nt = pd.concat([y_linreg_hat_train_nt, y_linreg_hat_test_nt])
 y_linreg_hat_nt = retime_SAR(y_linreg_hat_nt, X_full_all)
 
+# Linear Regression: no forest feature
+y_linreg_hat_train_nf = pd.read_pickle(
+    ml_obs_op_pad / 'lin_reg/full_data_no_forest/y_train_hat.pickle'
+)
+y_linreg_hat_test_nf = pd.read_pickle(
+    ml_obs_op_pad / 'lin_reg/full_data_no_forest/y_test_hat.pickle'
+)
+y_linreg_hat_nf = pd.concat([y_linreg_hat_train_nf, y_linreg_hat_test_nf])
+y_linreg_hat_nf = retime_SAR(y_linreg_hat_nf, X_full_all)
+
 # Ridge regression: window no time
 y_ridge_w_hat_train = pd.read_pickle(
     ml_obs_op_pad / 'ridge/window/y_train_hat.pickle')
@@ -66,6 +76,9 @@ y_linreg_hat.to_pickle(
     ml_obs_op_pad / 'lin_reg/full_data/y_hat_retimed.pickle')
 y_linreg_hat_nt.to_pickle(
     ml_obs_op_pad / 'lin_reg/full_data_no_time/y_hat_retimed.pickle')
+y_linreg_hat_nf.to_pickle(
+    ml_obs_op_pad / 'lin_reg/full_data_no_forest/y_hat_retimed.pickle'
+)
 y_ridge_w_hat.to_pickle(
     ml_obs_op_pad / 'ridge/window/y_hat_retimed.pickle')
 y_SVR_lin_hat.to_pickle(
